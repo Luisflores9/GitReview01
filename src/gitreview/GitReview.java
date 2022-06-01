3e73d44ded0f11c8ade3e8692bad6eddec55b647
Luisflores9 <Luis.fsanchez09@gmail.com>
2022-05-31 21:35:38 -0400

    1st commit

---
+++ src/gitreview/GitReview.java
@@ -0,0 +1,12 @@
+package gitreview;
+
+public class GitReview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Chelsea FC will win the Champions League next year");
+		
+
+	}
+
+}
